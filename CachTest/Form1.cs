9aec6355ecb12ce53e52468fe689cfba884f2121
Shahin azad <Shahin azad>
2017-04-07 01:07:15 +0430

    Created Project

---
+++ CachTest/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CacheSystem;
+using System.Windows.Forms;
+
+namespace CachTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cache c = new Cache("C:\\Users\\Shahin\\Desktop", "TEST_CACHE");
+            Console.WriteLine(c.DownloadFile("https://www.tutorialspoint.com/green/images/logo.png", "7C36F81013CE9E0A169D43DAAF98BD21"));
+            Console.WriteLine(c.GetMD5(c.isFileCached("7C36F81013CE9E0A169D43DAAF98BD21")));
+        }
+    }
+}
---
+++ CacheSystem/Cache.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace CacheSystem
+{
+    public class CacheResponses
+    {
+        public const string FILE_NOT_CACHED = "FILE_NOT_CACHED";
+        public const string FILE_DOWNLOAD_ERROR = "FILE_DOWNLOAD_ERROR";
+    }
+    public class Cache
+    {
+        private string BASE;
+        private string FOLDER_NAME;
+        public Exception LastExeption;
+        public Cache(string Base,string FolderName)
+        {
+            FOLDER_NAME = FolderName;
+            string tmpPath = Base + "\\" + FolderName;
+
+            if (!Directory.Exists(tmpPath))
+            {
+                //Console.WriteLine("Directory Does Not Exist");
+                Directory.CreateDirectory(tmpPath);
+                //Console.WriteLine("Create One, {0}", tmpPath);
+            }
+            BASE = tmpPath;
+        }
+
+        public string isFileCached(string MD5)
+        {
+            string tmpFilePath = BASE + "\\" + MD5.ToLower();
+            if (File.Exists(tmpFilePath))
+            {
+                //Console.WriteLine("File({0}) Exist , PATH={1}",MD5, tmpFilePath);
+                return tmpFilePath;
+            }
+            else
+            {
+                //Console.WriteLine("File Does Not Cached Before");
+                return "FILE_NOT_CACHED";
+            }
+        }
+
+        public string DownloadFile(string FileURL,string MD5)
+        {
+            try
+            {
+                string res = isFileCached(MD5);
+                if (res ==CacheResponses.FILE_NOT_CACHED)
+                {
+                    WebClient client = new WebClient();
+                    string tmpFilePath = BASE + "\\" + MD5.ToLower();
+                    client.DownloadFile(new Uri(FileURL), tmpFilePath);
+                    //Console.WriteLine("File Downloaded ,PATH={0}", tmpFilePath);
+                    return tmpFilePath;
+                }
+                else
+                {
+                    return res;
+                }
+            }catch(Exception ex)
+            {
+                //Console.WriteLine("ERROR: "+ex.Message);
+                LastExeption = ex;
+                return CacheResponses.FILE_DOWNLOAD_ERROR;
+            }
+        }
+
+        public string GetMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", String.Empty).ToLower();
+                }
+            }
+        }
+
+    }
+}
